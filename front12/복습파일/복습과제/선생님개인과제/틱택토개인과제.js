c10185f09a5200c043a2bf0b1854fb4ea40554fa
김경주 <@192.168.219.110>
2022-08-19 01:07:59 +0900

    대각선추가

--- front12/복습파일/복습과제/선생님개인과제/틱택토개인과제.js
+++ front12/복습파일/복습과제/선생님개인과제/틱택토개인과제.js
@@ -6,33 +6,82 @@
 */
 
 
-
-
-
-function 게임판그리기(){ //fun s
-   let 게임판=''
-   let 게임상황=[null,null,null,null,null,null,null,null,null]
-   for(let i = 0; i<9; i++ ){ //for s
-	 게임판 +=`<button id=${i} onclick=버튼선택(${i})>${i}</button>`
-	 if(i % 3== 2){게임판+=`<br>`}
-	}//for e
-	document.getElementById('gameboard').innerHTML = 게임판
-}//fun e
-function 버튼선택(i){//fun s
-	 if(중복검사(i)){//if s
-		alert('이미둔자리입니다.')
+let 승리=''
+let 게임상황=[]
+function 게임판그리기(){
+		
+		let 게임판=''
+		게임상황=[null,null,null,null,null,null,null,null,null]
+		승리 = ''
+		for(let i = 0; i<9; i++){
+			게임판+=`<button id=${i} onclick='버튼선택(${i})'>${i}</button>`
+			if(i % 3==2){게임판+=`<br>`}
+		}
+		document.getElementById('gameboard').innerHTML = 게임판
+}
+function 버튼선택( i ){
+	
+	if (중복검사(i)) {
+		alert('중복입니다.')
 		return
-	}//if e
-	 document.getElementById(i).innerHTML ='o'
+	}
+	document.getElementById(i).innerHTML = 'o'
 	게임상황[i] = 'o'
-}//fun e
-function 중복검사( i  ){
-	if( 게임상황[i] != null ){ // 배열은 모두 null로 초기화했기때문에 null 아니면 'O'또는'X'가 존재하는 의미 [ 알이 있다. ]
-		return true // 알이 있다.  null 이 아니면 
+	
+	if(게임결과()){
+		alert('승리입니다.:'+승리)
+		return
+	}
+
+	while (true) {
+		let 난수 = Math.floor(Math.random() * 9)
+		if (!중복검사(난수)) {
+			
+			document.getElementById(난수).innerHTML = 'x'
+			게임상황[난수] = 'x'
+			break
+		}
+
+	}
+	if(게임결과()){
+		alert('승리입니다.:'+승리)
+		return
+	}
+	
+}
+function 중복검사(i){
+	if(게임상황[i] !=null){
+	   return true
 	}else{
-		return false // 알이 없다.  null 이면 
-	} 
+		return false
+	}
 }
 
-//선생님 한테 질문 : 이해를 위해 중복검사 버튼을 다시 눌렀을떄 중복이 뜨는 순서와 과정
-//모든 해석의 순서 
+function 게임결과(){
+	for (let i = 0; i<=6; i+=3){
+		if(게임상황[i]!=null && 게임상황[i]==게임상황[i+1]&&게임상황[i+1]==게임상황[i+2] ){
+			승리 = 게임상황[i]
+			return true
+		}
+	}
+
+
+	for(let i=0; i<=2; i++){
+		if(게임상황[i]!=null && 게임상황[i]==게임상황[i+3]&&게임상황[i+3]==게임상황[i+6]){
+		 	승리 =게임상황[i]
+		 	return true
+		 	}
+		}
+	for(let i=0; i<=8; i+=2){
+		if(게임상황[i]!=null &&게임상황[i]==게임상황[i+2]&&게임상황[i+2]==게임상황[i+8]){
+			승리=게임상황[i]
+			return true
+		}
+	}
+	
+}
+
+
+
+
+
