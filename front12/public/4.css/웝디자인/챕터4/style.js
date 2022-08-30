9ec885794d4f6fc73ec2142860073617b3b1efca
504 <504@DESKTOP-9CL6RRI>
2022-08-30 18:16:07 +0900

    1

--- front12/public/4.css/웝디자인/챕터4/style.js
+++ front12/public/4.css/웝디자인/챕터4/style.js
@@ -3,7 +3,7 @@
  	변수=값
  	배열 = [값1ㅡ값2]
  	객체 = {키:값, 키2:값2}
- */	
+ 	*/
  //객체
  /*파이차트[원형차트]데이터 객체 선언*/
  let pieData = {
@@ -71,3 +71,42 @@ bardata2.series[0].push(money)
 new Chartist.Bar('.bar_chart2',bardata2,baroptions2)
 }
 //new 클래스명(): 
+/*-----------------------------------*/
+/*
+	0.전송버튼을 눌렀을때 (클릭) 이벤트 함수실행
+	1.html 에서 입력된 데이터 js 가져와서 변수/ 객체 저장 ->확인
+	2. documant.quaryselector() 
+	
+
+*/
+
+let 지원자리스트 = [] /*지원자 객체들을 담을 배열 선언:*/
+
+function 전송(){
+	console.log('버튼눌림')
+	let 입력리스트 = document.querySelectorAll('input')
+	console.log('입력요소들')
+	let	목록상자 = document.querySelector('select')
+	let 긴글상자 = document.querySelector('textarea')
+	//2번테스트	
+	console.log(입력리스트)
+	console.log(목록상자)
+	console.log(긴글상자)
+	
+	let 지원자 = {
+		성명 : 입력리스트[0].value,
+		영문명 : 입력리스트[1].value,
+		메일주소 :입력리스트[2].value,
+		포트폴리오 : 입력리스트[3].value,
+		희망근무지 :입력리스트[4].value,
+		현재상태 : 목록상자.value ,
+		자기소개 : 긴글상자.value ,
+	}
+	console.log(지원자)
+	지원자리스트.push(지원자)
+	console.log(지원자리스트)	
+}
+
+
+
+
