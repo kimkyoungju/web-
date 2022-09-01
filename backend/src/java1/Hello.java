bc4f534a58065294b6032877bacf851d3df20749
504 <504@DESKTOP-9CL6RRI>
2022-09-01 12:27:16 +0900

    back

---
+++ backend/src/java1/Hello.java
@@ -0,0 +1,26 @@
+package java1; //현재 클래스가 속한 패키지[폴더] 위치 표시 
+
+//1. 클래스 선언부 
+public class Hello {	//class  s
+//1	public : 전범위 접근제한자 (전범위)
+		//class : 클래스 선언시 사용되는 키워드
+			//hello : 클래스명[아무거나]
+				//{} : 클래스 시작~끝
+		//*모든 코드는 클래스 {} 안에서 작성해야 된다 !!
+	
+	//main + 자동완성
+	
+	//2. 함수 선언부
+	public static void main(String[] args) {
+					//main 메소드에 main스레드[코드를 읽어주는 역활]
+					// * 실행할 코드 main 메소드 안에서 작성 
+		//syso + 자동완성
+		System.out.println("Hello , Java");
+		//syso 자동완성 
+	}	
+	
+	//3. 주석은 같음
+	//4.세미클론;
+		//싫행문이 끝났음을 표시 =----> 실행
+		// { } x 	/if(); {} x    /for() ; {} x
+} //class e
