21bfc8c50b0333e74d6029b71ba96f7623e653ad
504 <504@DESKTOP-A1HFHNV>
2022-10-14 19:59:29 +0900

    ==

--- 과제/src/main/java/sql.sql
+++ 과제/src/main/java/sql.sql
@@ -8,13 +8,15 @@ title varchar(100),
 writer varchar(100),
 content varchar(100),
 pw varchar(100),
-day datetime,
-view int
+day datetime  default now(),
+view int default 0
 
 
 
 
 );
 insert into board (title,writer,content,pw) values('1','1','1','1');
-select * from board;
+select  title,writer,content,day,view from board where num = 2;
+select  title,writer,content,day,view from board where num = 1;
+select* from board;
 
